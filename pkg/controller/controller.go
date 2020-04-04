a7d013da5e3b9df0f0b8a6f566c3e914884e09d7
Roi Vazquez <roivazquez@gmail.com>
2020-04-04 15:26:50 +0200

    Adjust tls config for ads server

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -49,6 +49,23 @@ func NewController(
 		gatewayPort,
 		managementPort,
 		&tls.Config{
+			MinVersion:               tls.VersionTLS12,
+			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+			PreferServerCipherSuites: true,
+			CipherSuites: []uint16{
+				// Sadly, these 2 are required to use http2 and
+				// must go first as others could be offered first
+				// to the client and cause the connection to be
+				// rejected
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+
+				// This are for the management gateway
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			},
 			// TODO: mechanism to reload server certificate when renewed
 			// Probably the easieast way is to have a goroutine force server
 			// graceful shutdown when it detects the certificate has changed
