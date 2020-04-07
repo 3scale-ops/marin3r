b8ccf3e820d55d349e74473e3580da26bb8026e8
Roi Vazquez <roivazquez@gmail.com>
2020-04-07 16:36:51 +0200

    More tests for the reconcile loop

--- pkg/reconciler/reconciler_test.go
+++ pkg/reconciler/reconciler_test.go
@@ -15,85 +15,176 @@
 package reconciler
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 	"time"
 
+	envoyapi "github.com/envoyproxy/go-control-plane/envoy/api/v2"
 	envoyauth "github.com/envoyproxy/go-control-plane/envoy/api/v2/auth"
+	envoysd "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v2"
 	"github.com/envoyproxy/go-control-plane/pkg/cache"
-	"github.com/roivaz/marin3r/pkg/envoy"
 	"go.uber.org/zap"
 	"k8s.io/client-go/kubernetes"
 )
 
-func testReconciler() *Reconciler {
+func testReconciler(rcaches caches) *Reconciler {
 	lg, _ := zap.NewDevelopment()
 	logger := lg.Sugar()
 	stopper := make(chan struct{})
 	scache := cache.NewSnapshotCache(true, cache.IDHash{}, nil)
-	return NewReconciler(&kubernetes.Clientset{}, "xxxx", &scache, stopper, logger)
+	var rc caches
+	// return NewReconciler(&kubernetes.Clientset{}, "xxxx", &scache, stopper, logger)
+	if rcaches != nil {
+		rc = rcaches
+	} else {
+		rc = make(caches)
+	}
+
+	return &Reconciler{
+		clientset:     &kubernetes.Clientset{},
+		namespace:     "namespace",
+		caches:        rc,
+		snapshotCache: &scache,
+		Queue:         make(chan ReconcileJob),
+		logger:        logger,
+		stopper:       stopper,
+	}
 }
 
 type testJob struct {
-	nodeID string
+	nodeIDs []string
+	fail    bool
+	panic   bool
 }
 
 func (job testJob) Push(queue chan ReconcileJob) { queue <- job }
 func (job testJob) process(c caches, clientset *kubernetes.Clientset, namespace string, logger *zap.SugaredLogger) ([]string, error) {
-	c[job.nodeID] = NewNodeCaches()
-	c[job.nodeID].secrets = map[string]*envoyauth.Secret{"test-secret": envoy.NewSecret("test-secret", "xxxx", "xxxx")}
-	return []string{job.nodeID}, nil
+
+	// Simulate a job panic
+	if job.fail {
+		panic("job failed")
+	}
+	// Simulate a job failure
+	if job.fail {
+		return []string{}, fmt.Errorf("job failed")
+	}
+
+	// Simulate a successful job
+	for _, nodeID := range job.nodeIDs {
+		c[nodeID] = NewNodeCaches()
+		c[nodeID].secrets[fmt.Sprintf("%s-secret", nodeID)] = &envoyauth.Secret{Name: fmt.Sprintf("%s-secret", nodeID)}
+	}
+
+	return job.nodeIDs, nil
 }
 
 func TestReconciler_RunReconciler(t *testing.T) {
 
-	t.Run("Stops goroutine on stopper channel closed", func(t *testing.T) {
-		rec := testReconciler()
-		close(rec.stopper)
-		rec.RunReconciler()
-	})
+	type args struct {
+		jobs []testJob
+	}
 
-	t.Run("Processes a test job and generates snapshot", func(t *testing.T) {
-		rec := testReconciler()
+	type want struct {
+		nodeID string
+		value  map[string]cache.Resource
+	}
+
+	reconcileLoopTestFn := func(jobs []testJob, want []want) {
+		rec := testReconciler(nil)
 
 		go func() {
 			rec.RunReconciler()
 		}()
 
-		job := testJob{nodeID: "test-node"}
-		job.Push(rec.Queue)
-		time.Sleep(50 * time.Millisecond)
+		// Push all jobs to the queue
+		for _, job := range jobs {
+			job.Push(rec.Queue)
+		}
 
-		// Validate that the job was processed by checking that the wanted secret
-		// exists in the snapshotCache
-		snap, err := (*rec.snapshotCache).GetSnapshot("test-node")
-		want := envoy.NewSecret("test-secret", "xxxx", "xxxx")
-		if err != nil {
-			t.Fatal("error recovering processed cache")
+		// Wait for jobs to be processes
+		time.Sleep(1 * time.Second)
+
+		// Check if the final status of the snapshotCache is the wanted
+		for _, w := range want {
+
+			snap, err := (*rec.snapshotCache).GetSnapshot(w.nodeID)
+			if err != nil {
+				t.Fatalf("error recovering processed cache for node %s: '%s'", w.nodeID, err)
+			}
+			got := snap.Resources[cache.Secret].Items
+			if !reflect.DeepEqual(got, w.value) {
+				t.Errorf("RunReconciler() = '%v', want '%v'", got, w.value)
+			}
 		}
-		if got := snap.Resources[cache.Secret].Items["test-secret"]; !reflect.DeepEqual(got, want) {
-			t.Errorf("RunReconciler() = '%v', want '%v'", snap.Resources[cache.Secret].Items["test-secret"], want)
-		}
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want []want
+	}{
+		{
+			"Processes a job for a single nodeID and generates the expected snapshot",
+			args{[]testJob{
+				testJob{nodeIDs: []string{"node"}, fail: false, panic: false}},
+			},
+			[]want{
+				want{"node", map[string]cache.Resource{"node-secret": &envoyauth.Secret{Name: "node-secret"}}},
+			},
+		},
+		{
+			"Processes a job for a several nodeIDs and generates the expected snapshots",
+			args{[]testJob{
+				testJob{nodeIDs: []string{"node1", "node2"}, fail: false, panic: false},
+			}},
+			[]want{
+				want{"node1", map[string]cache.Resource{"node1-secret": &envoyauth.Secret{Name: "node1-secret"}}},
+				want{"node2", map[string]cache.Resource{"node2-secret": &envoyauth.Secret{Name: "node2-secret"}}},
+			},
+		}, {
+			"Processes a job that returns error without altering the snapshotCache",
+			args{[]testJob{
+				testJob{nodeIDs: []string{"node"}, fail: false, panic: false},
+				testJob{nodeIDs: []string{"node"}, fail: true, panic: false},
+			}},
+			[]want{
+				want{"node", map[string]cache.Resource{"node-secret": &envoyauth.Secret{Name: "node-secret"}}},
+			},
+		}, {
+			"Keeps processing jobs after one job panics",
+			args{[]testJob{
+				testJob{nodeIDs: []string{"node"}, fail: false, panic: true},
+				testJob{nodeIDs: []string{"node"}, fail: false, panic: false},
+			}},
+			[]want{
+				want{"node", map[string]cache.Resource{"node-secret": &envoyauth.Secret{Name: "node-secret"}}},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) { reconcileLoopTestFn(tt.args.jobs, tt.want) })
+	}
+
+	t.Run("Stops goroutine on stopper channel closed", func(t *testing.T) {
+		rec := testReconciler(nil)
+		close(rec.stopper)
+		rec.RunReconciler()
 	})
-
-	// TODO cases
-	t.Run("Generates snapshot for each nodeID in the list returned by 'process'", func(t *testing.T) {})
-	t.Run("Does not generate snapshot on 'process' error", func(t *testing.T) {})
 
 }
 
 func TestReconciler_runStopWatcher(t *testing.T) {
-	tests := []struct {
-		name string
-		r    *Reconciler
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tt.r.runStopWatcher()
-		})
-	}
+
+	t.Run("Closes Reconciler.Queue channel when receives the stopper signal", func(t *testing.T) {
+		rec := testReconciler(nil)
+		close(rec.stopper)
+		go func() {
+			rec.runStopWatcher()
+		}()
+		<-rec.Queue
+	})
 }
 
 func TestReconciler_makeSnapshot(t *testing.T) {
@@ -104,12 +195,54 @@ func TestReconciler_makeSnapshot(t *testing.T) {
 		name string
 		r    *Reconciler
 		args args
+		want cache.Snapshot
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Generate new snapshot with listeners",
+			r: testReconciler(map[string]*nodeCaches{
+				"node1": &nodeCaches{
+					secrets: map[string]*envoyauth.Secret{},
+					listeners: map[string]*envoyapi.Listener{
+						"listener1": &envoyapi.Listener{Name: "listener1"},
+					},
+					clusters: map[string]*envoyapi.Cluster{},
+					endpoint: map[string]*envoyapi.ClusterLoadAssignment{},
+					runtime:  map[string]*envoysd.Runtime{},
+				},
+			}),
+			args: args{nodeID: "node1"},
+			want: cache.Snapshot{
+				Resources: [6]cache.Resources{
+					cache.Resources{Version: "1", Items: map[string]cache.Resource{}}, // cache.Enspoint
+					cache.Resources{Version: "1", Items: map[string]cache.Resource{}}, // cache.Cluster
+					cache.Resources{Version: "1", Items: map[string]cache.Resource{}}, // cache.Route
+					cache.Resources{Version: "1", Items: map[string]cache.Resource{
+						"listener1": &envoyapi.Listener{Name: "listener1"},
+					}},
+					cache.Resources{Version: "1", Items: map[string]cache.Resource{}}, // cache.Secret
+					cache.Resources{Version: "1", Items: map[string]cache.Resource{}}, // cache.Runtime
+				},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			tt.r.makeSnapshot(tt.args.nodeID)
+			got, err := (*tt.r.snapshotCache).GetSnapshot(tt.args.nodeID)
+			if err != nil {
+				t.Fatalf("error recovering processed cache for node %s: '%s'", tt.args.nodeID, err)
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("RunReconciler() = '%v', want '%v'", got, tt.want)
+			}
 		})
 	}
 }
+
+// func testReconciler() *Reconciler {
+// 	lg, _ := zap.NewDevelopment()
+// 	logger := lg.Sugar()
+// 	stopper := make(chan struct{})
+// 	scache := cache.NewSnapshotCache(true, cache.IDHash{}, nil)
+// 	return NewReconciler(&kubernetes.Clientset{}, "xxxx", &scache, stopper, logger)
+// }
