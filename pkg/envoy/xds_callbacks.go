49ac9f3bcf6c2b409b77a48298e4b1101c9b900f
Roi Vazquez <roivazquez@gmail.com>
2020-05-24 11:56:03 +0200

    Don't log secrets

--- pkg/envoy/xds_callbacks.go
+++ pkg/envoy/xds_callbacks.go
@@ -58,14 +58,13 @@ func (cb *Callbacks) OnStreamRequest(id int64, req *v2.DiscoveryRequest) error {
 // OnStreamResponse is called immediately prior to sending a response on a stream.
 func (cb *Callbacks) OnStreamResponse(id int64, req *v2.DiscoveryRequest, rsp *v2.DiscoveryResponse) {
 	resources := []string{}
-	// resourceNames := []string{}
 	for _, r := range rsp.Resources {
 		j, _ := ResourcesToJSON(r)
 		resources = append(resources, string(j))
 	}
-	if rsp.TypeUrl == "type.googleapis.com/envoy.api.v2.auth.Secretssss" {
+	if rsp.TypeUrl == "type.googleapis.com/envoy.api.v2.auth.Secret" {
 		logger.V(1).Info("Response sent to gateway",
-			"Resources", "Resources are secrets, refusing to log", "TypeURL", req.TypeUrl, "NodeID", req.Node.Id, "StreamID", id)
+			"ResourcesNames", req.ResourceNames, "TypeURL", req.TypeUrl, "NodeID", req.Node.Id, "StreamID", id)
 	} else {
 		logger.V(1).Info("Response sent to gateway",
 			"Resources", resources, "TypeURL", req.TypeUrl, "NodeID", req.Node.Id, "StreamID", id)
