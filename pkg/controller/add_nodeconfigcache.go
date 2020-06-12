799597995051673b174c910a83a4e70a8b074eb7
Roi Vazquez <roivazquez@gmail.com>
2020-06-12 17:00:28 +0200

    Change how controllers are added to the manager

--- pkg/controller/add_nodeconfigcache.go
+++
@@ -1,6 +0,0 @@
-package controller
-
-func init() {
-	// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
-	// AddToManagerFuncs = append(AddToManagerFuncs, nodeconfigcache.Add)
-}
