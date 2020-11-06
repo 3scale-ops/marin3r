528ff31ea2daf532f0d5d79de51709b7c150ce2f
Roi Vazquez <roivazquez@gmail.com>
2020-11-06 12:40:51 +0100

    Add tests

---
+++ pkg/envoy/resources/v3/type_urls_test.go
@@ -0,0 +1,34 @@
+package envoy
+
+import (
+	"reflect"
+	"testing"
+
+	envoy_resources "github.com/3scale/marin3r/pkg/envoy/resources"
+)
+
+func TestMappings(t *testing.T) {
+	tests := []struct {
+		name string
+		want map[envoy_resources.Type]string
+	}{
+		{
+			name: "Returns the typeURL to resource types mapping",
+			want: map[envoy_resources.Type]string{
+				"Listener": "type.googleapis.com/envoy.config.listener.v3.Listener",
+				"Route":    "type.googleapis.com/envoy.config.route.v3.RouteConfiguration",
+				"Cluster":  "type.googleapis.com/envoy.config.cluster.v3.Cluster",
+				"Endpoint": "type.googleapis.com/envoy.config.endpoint.v3.ClusterLoadAssignment",
+				"Secret":   "type.googleapis.com/envoy.extensions.transport_sockets.tls.v3.Secret",
+				"Runtime":  "type.googleapis.com/envoy.service.runtime.v3.Runtime",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Mappings(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Mappings() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
