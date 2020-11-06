1005a79e597b3d17f50c76cfb1b08b423ed50dfc
Roi Vazquez <roivazquez@gmail.com>
2020-11-06 11:34:16 +0100

    Use exported types from go-control-plane instead of hardcoding the type URLs

--- pkg/envoy/resources/v2/type_urls.go
+++ pkg/envoy/resources/v2/type_urls.go
@@ -2,17 +2,18 @@ package envoy
 
 import (
 	envoy_resources "github.com/3scale/marin3r/pkg/envoy/resources"
+	resource_v2 "github.com/envoyproxy/go-control-plane/pkg/resource/v2"
 )
 
 // Mappings return a map associating "github.com/3scale/marin3r/pkg/envoy/resources".Type to
 // the v2 envoy API type URLs for each resource type
 func Mappings() map[envoy_resources.Type]string {
 	return map[envoy_resources.Type]string{
-		envoy_resources.Listener: "type.googleapis.com/envoy.api.v2.Listener",
-		envoy_resources.Route:    "type.googleapis.com/envoy.api.v2.RouteConfiguration",
-		envoy_resources.Cluster:  "type.googleapis.com/envoy.api.v2.Cluster",
-		envoy_resources.Endpoint: "type.googleapis.com/envoy.api.v2.ClusterLoadAssignment",
-		envoy_resources.Secret:   "type.googleapis.com/envoy.api.v2.auth.Secret",
-		envoy_resources.Runtime:  "type.googleapis.com/envoy.service.discovery.v2.Runtime",
+		envoy_resources.Listener: resource_v2.ListenerType,
+		envoy_resources.Route:    resource_v2.RouteType,
+		envoy_resources.Cluster:  resource_v2.ClusterType,
+		envoy_resources.Endpoint: resource_v2.EndpointType,
+		envoy_resources.Secret:   resource_v2.SecretType,
+		envoy_resources.Runtime:  resource_v2.RuntimeType,
 	}
 }
--- pkg/envoy/resources/v3/type_urls.go
+++ pkg/envoy/resources/v3/type_urls.go
@@ -2,17 +2,18 @@ package envoy
 
 import (
 	envoy_resources "github.com/3scale/marin3r/pkg/envoy/resources"
+	resource_v3 "github.com/envoyproxy/go-control-plane/pkg/resource/v3"
 )
 
 // Mappings return a map associating "github.com/3scale/marin3r/pkg/envoy/resources".Type to
 // the v3 envoy API type URLs for each resource type
 func Mappings() map[envoy_resources.Type]string {
 	return map[envoy_resources.Type]string{
-		envoy_resources.Listener: "type.googleapis.com/envoy.config.listener.v3.Listener",
-		envoy_resources.Route:    "type.googleapis.com/envoy.config.route.v3.RouteConfiguration",
-		envoy_resources.Cluster:  "type.googleapis.com/envoy.config.cluster.v3.Cluster",
-		envoy_resources.Endpoint: "type.googleapis.com/envoy.config.endpoint.v3.ClusterLoadAssignment",
-		envoy_resources.Secret:   "type.googleapis.com/envoy.extensions.transport_sockets.tls.v3.Secret",
-		envoy_resources.Runtime:  "type.googleapis.com/envoy.service.runtime.v3.Runtime",
+		envoy_resources.Listener: resource_v3.ListenerType,
+		envoy_resources.Route:    resource_v3.RouteType,
+		envoy_resources.Cluster:  resource_v3.ClusterType,
+		envoy_resources.Endpoint: resource_v3.EndpointType,
+		envoy_resources.Secret:   resource_v3.SecretType,
+		envoy_resources.Runtime:  resource_v3.RuntimeType,
 	}
 }
