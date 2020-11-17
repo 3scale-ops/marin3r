7db0c813262bb22d1b933f77d320cf7e18e27222
Roi Vazquez <roivazquez@gmail.com>
2020-11-17 15:59:11 +0100

    Port operator group tests to envtest

--- controllers/operator/discoveryservice_controller_test.go
+++ controllers/operator/discoveryservice_controller_test.go
@@ -2,131 +2,235 @@ package controllers
 
 import (
 	"context"
-	"testing"
+	"time"
 
+	envoyv1alpha1 "github.com/3scale/marin3r/apis/envoy/v1alpha1"
 	operatorv1alpha1 "github.com/3scale/marin3r/apis/operator/v1alpha1"
-	admissionregistrationv1beta1 "k8s.io/api/admissionregistration/v1beta1"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	admissionregistrationv1beta1 "k8s.io/api/admissionregistration/v1"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/client-go/kubernetes/scheme"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client/fake"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
-var s *runtime.Scheme = scheme.Scheme
+var _ = Describe("EnvoyConfigRevision controller", func() {
+	var namespace string
+	var ds *operatorv1alpha1.DiscoveryService
 
-func init() {
-	s.AddKnownTypes(operatorv1alpha1.GroupVersion,
-		&operatorv1alpha1.DiscoveryService{},
-		&operatorv1alpha1.DiscoveryServiceList{},
-		&operatorv1alpha1.DiscoveryServiceCertificate{},
-	)
-}
+	BeforeEach(func() {
+		// Create a namespace for each block
+		namespace = "test-ns-" + nameGenerator.Generate()
 
-func TestReconcileDiscoveryService_Reconcile(t *testing.T) {
+		// Add any setup steps that needs to be executed before each test
+		testNamespace := &v1.Namespace{
+			TypeMeta:   metav1.TypeMeta{APIVersion: "v1", Kind: "Namespace"},
+			ObjectMeta: metav1.ObjectMeta{Name: namespace},
+		}
 
-	t.Run("Creates a new DiscoveryService", func(t *testing.T) {
-		ds := &operatorv1alpha1.DiscoveryService{
-			ObjectMeta: metav1.ObjectMeta{Name: "instance"},
+		err := k8sClient.Create(context.Background(), testNamespace)
+		Expect(err).ToNot(HaveOccurred())
+
+		n := &v1.Namespace{}
+		Eventually(func() bool {
+			err := k8sClient.Get(context.Background(), types.NamespacedName{Name: namespace}, n)
+			if err != nil {
+				return false
+			}
+			return true
+		}, 30*time.Second, 5*time.Second).Should(BeTrue())
+
+		By("creating a DiscoveryService instance")
+		ds = &operatorv1alpha1.DiscoveryService{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "instance",
+			},
 			Spec: operatorv1alpha1.DiscoveryServiceSpec{
 				Image:                     "image",
-				DiscoveryServiceNamespace: "default",
-				EnabledNamespaces:         []string{"default"},
+				DiscoveryServiceNamespace: namespace,
+				EnabledNamespaces:         []string{namespace},
 			},
 		}
+		err = k8sClient.Create(context.Background(), ds)
+		Expect(err).ToNot(HaveOccurred())
+		Eventually(func() bool {
+			err := k8sClient.Get(context.Background(), types.NamespacedName{Name: "instance"}, ds)
+			if err != nil {
+				return false
+			}
+			return true
+		}, 30*time.Second, 5*time.Second).Should(BeTrue())
 
-		// Fake namespace
-		ns := &corev1.Namespace{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: "default",
-			},
-		}
-		// Fake certificates that should be created by
-		// the discoveryservicecertificate controller
-		serverCert := &corev1.Secret{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "marin3r-server-cert-instance",
-				Namespace: "default",
-			},
-			Type: corev1.SecretTypeTLS,
-			Data: map[string][]byte{
-				"tls.key": []byte("xxxxx"),
-				"tls.crt": []byte("xxxxx"),
-			},
-		}
-		caCert := &corev1.Secret{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "marin3r-ca-cert-instance",
-				Namespace: "default",
-			},
-			Type: corev1.SecretTypeTLS,
-			Data: map[string][]byte{
-				"tls.key": []byte("xxxxx"),
-				"tls.crt": []byte("xxxxx"),
-			},
-		}
-
-		r := &DiscoveryServiceReconciler{
-			Client: fake.NewFakeClient(ns, ds, serverCert, caCert),
-			Scheme: s,
-			Log:    ctrl.Log.WithName("test"),
-		}
-		req := reconcile.Request{
-			NamespacedName: types.NamespacedName{
-				Name:      "instance",
-				Namespace: "",
-			},
-		}
-
-		_, gotErr := r.Reconcile(req)
-		if gotErr != nil {
-			t.Errorf("ReconcileDiscoveryService.Reconcile() error = %v", gotErr)
-			return
-		}
-
-		// --------------------------------------------
-		// Validate that all objects have been created
-		// --------------------------------------------
-
-		// DiscoveryServiceCertificates are created
-		dscCA := &operatorv1alpha1.DiscoveryServiceCertificate{}
-		if r.Client.Get(context.TODO(), types.NamespacedName{Name: getCACertName(ds), Namespace: "default"}, dscCA) != nil {
-			t.Errorf("The DiscoveryServiceCertificate object for the CA certificate is missing: %v", getCACertName(ds))
-		}
-		dscServer := &operatorv1alpha1.DiscoveryServiceCertificate{}
-		if r.Client.Get(context.TODO(), types.NamespacedName{Name: getServerCertName(ds), Namespace: "default"}, dscServer) != nil {
-			t.Errorf("The DiscoveryServiceCertificate object for the server certificate is missing: %v", getServerCertName(ds))
-		}
-
-		// All discovery service Deployment related objects are created: ServiceAccount, ClusterRole,
-		// ClusterRoleBinding and Deployment
-		sa := &corev1.ServiceAccount{}
-		if r.Client.Get(context.TODO(), types.NamespacedName{Name: OwnedObjectName(ds), Namespace: "default"}, sa) != nil {
-			t.Errorf("The ServiceAccount object for the discovery service Deployment is missing: %v", OwnedObjectName(ds))
-		}
-		cr := &rbacv1.ClusterRole{}
-		if r.Client.Get(context.TODO(), types.NamespacedName{Name: OwnedObjectName(ds)}, cr) != nil {
-			t.Errorf("The ClusterRole object for the discovery service Deployment is missing: %v", OwnedObjectName(ds))
-		}
-		crb := &rbacv1.ClusterRoleBinding{}
-		if r.Client.Get(context.TODO(), types.NamespacedName{Name: OwnedObjectName(ds)}, crb) != nil {
-			t.Errorf("The ClusterRoleBinding object for the discovery service Deployment is missing: %v", OwnedObjectName(ds))
-		}
-		dep := &appsv1.Deployment{}
-		if r.Client.Get(context.TODO(), types.NamespacedName{Name: OwnedObjectName(ds), Namespace: "default"}, dep) != nil {
-			t.Errorf("The Deployment object for the discovery service is missing: %v", OwnedObjectName(ds))
-		}
-
-		mwc := &admissionregistrationv1beta1.MutatingWebhookConfiguration{}
-		if r.Client.Get(context.TODO(), types.NamespacedName{Name: OwnedObjectName(ds)}, mwc) != nil {
-			t.Errorf("The MutatingWebhookConfiguration object is missing: %v", OwnedObjectName(ds))
-		}
-
-		// TODO: validate enabled namespaces
 	})
-}
+
+	Context("DiscoveryService", func() {
+
+		It("Create the required resources", func() {
+
+			By("waiting for the root CA DiscoveryServiceCertificate to be created")
+			{
+				dsc := &operatorv1alpha1.DiscoveryServiceCertificate{}
+				Eventually(func() bool {
+					if err := k8sClient.Get(
+						context.Background(),
+						types.NamespacedName{Name: getCACertName(ds), Namespace: namespace},
+						dsc,
+					); err != nil {
+						return false
+					}
+					return true
+				}, 30*time.Second, 5*time.Second).Should(BeTrue())
+
+				Expect(dsc.Spec.SecretRef.Name).To(Equal(ds.GetRootCertificateAuthorityOptions().SecretName))
+				Expect(dsc.Spec.ValidFor).To(Equal(int64(ds.GetRootCertificateAuthorityOptions().Duration.Seconds())))
+			}
+
+			By("waiting for the root CA DiscoveryServiceCertificate to be created")
+			{
+				dsc := &operatorv1alpha1.DiscoveryServiceCertificate{}
+				Eventually(func() bool {
+					if err := k8sClient.Get(
+						context.Background(),
+						types.NamespacedName{Name: getServerCertName(ds), Namespace: namespace},
+						dsc,
+					); err != nil {
+						return false
+					}
+					return true
+				}, 30*time.Second, 5*time.Second).Should(BeTrue())
+
+				Expect(dsc.Spec.SecretRef.Name).To(Equal(ds.GetServerCertificateOptions().SecretName))
+				Expect(dsc.Spec.ValidFor).To(Equal(int64(ds.GetServerCertificateOptions().Duration.Seconds())))
+			}
+
+			By("waiting for the discovery service ServiceAccount to be created")
+
+			{
+				sa := &corev1.ServiceAccount{}
+				Eventually(func() bool {
+					if err := k8sClient.Get(
+						context.Background(),
+						types.NamespacedName{Name: OwnedObjectName(ds), Namespace: namespace},
+						sa,
+					); err != nil {
+						return false
+					}
+					return true
+				}, 30*time.Second, 5*time.Second).Should(BeTrue())
+			}
+
+			By("waiting for the discovery service ClusterRole to be created")
+
+			{
+				cr := &rbacv1.ClusterRole{}
+				Eventually(func() bool {
+					if err := k8sClient.Get(
+						context.Background(),
+						types.NamespacedName{Name: OwnedObjectName(ds)},
+						cr,
+					); err != nil {
+						return false
+					}
+					return true
+				}, 30*time.Second, 5*time.Second).Should(BeTrue())
+			}
+
+			By("waiting for the discovery service ClusterRoleBinding to be created")
+
+			{
+				crb := &rbacv1.ClusterRoleBinding{}
+				Eventually(func() bool {
+					if err := k8sClient.Get(
+						context.Background(),
+						types.NamespacedName{Name: OwnedObjectName(ds)},
+						crb,
+					); err != nil {
+						return false
+					}
+					return true
+				}, 30*time.Second, 5*time.Second).Should(BeTrue())
+			}
+
+			By("waiting for the discovery service Deployment to be created")
+
+			{
+				dep := &appsv1.Deployment{}
+				Eventually(func() bool {
+					if err := k8sClient.Get(
+						context.Background(),
+						types.NamespacedName{Name: OwnedObjectName(ds), Namespace: namespace},
+						dep,
+					); err != nil {
+						return false
+					}
+					return true
+				}, 30*time.Second, 5*time.Second).Should(BeTrue())
+			}
+
+			By("waiting for the discovery service Service to be created")
+
+			{
+				svc := &corev1.Service{}
+				Eventually(func() bool {
+					if err := k8sClient.Get(
+						context.Background(),
+						types.NamespacedName{Name: ds.GetServiceConfig().Name, Namespace: namespace},
+						svc,
+					); err != nil {
+						return false
+					}
+					return true
+				}, 30*time.Second, 5*time.Second).Should(BeTrue())
+			}
+
+			By("waiting for the discovery service MutatingWebhookConfiguration to be created")
+			{
+
+				mwc := &admissionregistrationv1beta1.MutatingWebhookConfiguration{}
+				Eventually(func() bool {
+					if err := k8sClient.Get(
+						context.Background(),
+						types.NamespacedName{Name: OwnedObjectName(ds)},
+						mwc,
+					); err != nil {
+						return false
+					}
+					return true
+				}, 30*time.Second, 5*time.Second).Should(BeTrue())
+			}
+
+			By("checking the namespaces in 'spec.enabledNamespaces' have the required label")
+
+			{
+				for _, name := range ds.Spec.EnabledNamespaces {
+					ns := &corev1.Namespace{}
+					Eventually(func() bool {
+						if err := k8sClient.Get(context.Background(), types.NamespacedName{Name: name}, ns); err != nil {
+							return false
+						}
+						return true
+					}, 30*time.Second, 5*time.Second).Should(BeTrue())
+					Expect(ns.Labels[operatorv1alpha1.DiscoveryServiceLabelKey]).To(Equal(ds.GetName()))
+				}
+			}
+
+			By("checking the namespaces in 'spec.enabledNamespaces' have an EnvoyBootstrap resource each")
+
+			{
+				for _, ns := range ds.Spec.EnabledNamespaces {
+					eb := &envoyv1alpha1.EnvoyBootstrap{}
+					Eventually(func() bool {
+						if err := k8sClient.Get(context.Background(), types.NamespacedName{Name: ds.GetName(), Namespace: ns}, eb); err != nil {
+							return false
+						}
+						return true
+					}, 30*time.Second, 5*time.Second).Should(BeTrue())
+				}
+			}
+		})
+	})
+
+})
