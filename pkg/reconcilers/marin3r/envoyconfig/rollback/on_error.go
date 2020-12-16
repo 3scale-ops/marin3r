4cc03e29e9c5142f49c978521b59b945194987e2
Roi Vazquez <roivazquez@gmail.com>
2020-12-16 19:55:08 +0100

    Refactor OnError() using new revision package funcitons. Add tests.

--- pkg/reconcilers/marin3r/envoyconfig/rollback/on_error.go
+++ pkg/reconcilers/marin3r/envoyconfig/rollback/on_error.go
@@ -7,9 +7,9 @@ import (
 	marin3rv1alpha1 "github.com/3scale/marin3r/apis/marin3r/v1alpha1"
 	envoy "github.com/3scale/marin3r/pkg/envoy"
 	"github.com/3scale/marin3r/pkg/reconcilers/marin3r/envoyconfig/filters"
+	"github.com/3scale/marin3r/pkg/reconcilers/marin3r/envoyconfig/revisions"
 
 	"github.com/operator-framework/operator-lib/status"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -24,28 +24,12 @@ func OnError(cl client.Client) func(nodeID, version, msg string, envoyAPI envoy.
 	return func(nodeID, version, msg string, envoyAPI envoy.APIVersion) error {
 
 		// Get the envoyconfig that corresponds to the envoy node that returned the error
-		ecrList := &marin3rv1alpha1.EnvoyConfigRevisionList{}
-		selector, err := metav1.LabelSelectorAsSelector(&metav1.LabelSelector{
-			MatchLabels: map[string]string{
-				filters.NodeIDTag:   nodeID,
-				filters.VersionTag:  version,
-				filters.EnvoyAPITag: string(envoyAPI),
-			},
-		})
+		ecr, err := revisions.Get(context.Background(), cl, "",
+			filters.ByNodeID(nodeID), filters.ByVersion(version), filters.ByEnvoyAPI(envoyAPI))
 		if err != nil {
 			return err
-		}
-		err = cl.List(context.TODO(), ecrList, &client.ListOptions{LabelSelector: selector})
-		if err != nil {
-			return err
-		}
-		if len(ecrList.Items) != 1 {
-			return fmt.Errorf("Got %v envoyconfigrevision objects when only 1 expected", len(ecrList.Items))
 		}
 
-		// Add the "ResourcesUpdateUnsuccessful" condition to the EnvoyConfigRevision object
-		// unless the condition is already set
-		ecr := &ecrList.Items[0]
 		if !ecr.Status.Conditions.IsTrueFor(marin3rv1alpha1.RevisionTaintedCondition) {
 			patch := client.MergeFrom(ecr.DeepCopy())
 			ecr.Status.Conditions.SetCondition(status.Condition{
@@ -55,7 +39,7 @@ func OnError(cl client.Client) func(nodeID, version, msg string, envoyAPI envoy.
 				Message: fmt.Sprintf("A gateway returned NACK to the discovery response: '%s'", msg),
 			})
 
-			if err := cl.Status().Patch(context.TODO(), ecr, patch); err != nil {
+			if err := cl.Status().Patch(context.Background(), ecr, patch); err != nil {
 				return err
 			}
 		}
