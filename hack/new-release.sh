0fbce76ff06e42d0d4085dd3e873bb0e9875f32e
Roi Vazquez <roivazquez@gmail.com>
2021-03-29 16:27:35 +0200

    Avoid publishing releases on devel versions

--- hack/new-release.sh
+++ hack/new-release.sh
@@ -3,7 +3,7 @@
 REPO="3scale/marin3r"
 
 # Skip if alpha release
-[[ ${1} == *"-alpha"* ]] && echo "" && exit 0
+[[ ${1} == *"-alpha"* ]] || [[ ${1} == *"-dev"* ]] && echo "" && exit 0
 
 # Skip if release already exists
 curl -o /dev/null --fail --silent "https://api.github.com/repos/${REPO}/releases/tags/${1}" && echo "" && exit 0
