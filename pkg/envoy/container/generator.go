e97098c0d79f4646aae7931b9a14a896aa39e9c6
Roi Vazquez <roivazquez@gmail.com>
2021-04-20 13:17:13 +0200

    Add the sutdown-manager as a sidecar container to envoy

--- pkg/envoy/container/generator.go
+++ pkg/envoy/container/generator.go
@@ -2,35 +2,40 @@ package envoy
 
 import (
 	"fmt"
+	"strings"
 
 	operatorv1alpha1 "github.com/3scale-ops/marin3r/apis/operator.marin3r/v1alpha1"
+	"github.com/3scale-ops/marin3r/pkg/version"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
 type ContainerConfig struct {
-	Name               string
-	Image              string
-	BootstrapConfigMap string
-	ConfigBasePath     string
-	ConfigFileName     string
-	ConfigVolume       string
-	TLSBasePath        string
-	TLSVolume          string
-	NodeID             string
-	ClusterID          string
-	ClientCertSecret   string
-	ExtraArgs          []string
-	Resources          corev1.ResourceRequirements
-	AdminPort          int32
-	Ports              []corev1.ContainerPort
-	LivenessProbe      operatorv1alpha1.ProbeSpec
-	ReadinessProbe     operatorv1alpha1.ProbeSpec
+	Name                     string
+	Image                    string
+	BootstrapConfigMap       string
+	ConfigBasePath           string
+	ConfigFileName           string
+	ConfigVolume             string
+	TLSBasePath              string
+	TLSVolume                string
+	NodeID                   string
+	ClusterID                string
+	ClientCertSecret         string
+	ExtraArgs                []string
+	Resources                corev1.ResourceRequirements
+	AdminPort                int32
+	Ports                    []corev1.ContainerPort
+	LivenessProbe            operatorv1alpha1.ProbeSpec
+	ReadinessProbe           operatorv1alpha1.ProbeSpec
+	ShutdownManagerEnabled   bool
+	ShutdownManagerPort      int32
+	ShutdownManagerResources corev1.ResourceRequirements
 }
 
 func (cc *ContainerConfig) Containers() []corev1.Container {
 
-	return []corev1.Container{{
+	containers := []corev1.Container{{
 		Name:    cc.Name,
 		Image:   cc.Image,
 		Command: []string{"envoy"},
@@ -94,6 +99,56 @@ func (cc *ContainerConfig) Containers() []corev1.Container {
 		TerminationMessagePolicy: corev1.TerminationMessageReadFile,
 		ImagePullPolicy:          corev1.PullIfNotPresent,
 	}}
+
+	if cc.ShutdownManagerEnabled {
+		containers = append(containers, corev1.Container{
+			Name:    "envoy-shutdown-manager",
+			Image:   strings.Join([]string{operatorv1alpha1.DefaultImageRegistry, version.Current()}, ":"),
+			Command: []string{},
+			Args:    []string{"shutdown-manager"},
+			Ports: []corev1.ContainerPort{{
+				Name:          "shutdown-manager",
+				ContainerPort: cc.ShutdownManagerPort,
+				Protocol:      corev1.ProtocolTCP,
+			}},
+			Resources: corev1.ResourceRequirements{},
+			LivenessProbe: &corev1.Probe{
+				Handler: corev1.Handler{
+					HTTPGet: &corev1.HTTPGetAction{
+						Path:   "/health",
+						Port:   intstr.FromInt(int(cc.ShutdownManagerPort)),
+						Scheme: corev1.URISchemeHTTP,
+					},
+				},
+				InitialDelaySeconds: 3,
+				PeriodSeconds:       10,
+			},
+			Lifecycle: &corev1.Lifecycle{
+				PreStop: &corev1.Handler{
+					HTTPGet: &corev1.HTTPGetAction{
+						Path:   "/drain",
+						Port:   intstr.FromInt(int(cc.ShutdownManagerPort)),
+						Scheme: corev1.URISchemeHTTP,
+					},
+				},
+			},
+			TerminationMessagePath:   corev1.TerminationMessagePathDefault,
+			TerminationMessagePolicy: corev1.TerminationMessageReadFile,
+			ImagePullPolicy:          corev1.PullIfNotPresent,
+		})
+
+		containers[0].Lifecycle = &corev1.Lifecycle{
+			PreStop: &corev1.Handler{
+				HTTPGet: &corev1.HTTPGetAction{
+					Path:   "/wait-for-drain",
+					Port:   intstr.FromInt(int(cc.ShutdownManagerPort)),
+					Scheme: corev1.URISchemeHTTP,
+				},
+			},
+		}
+	}
+
+	return containers
 }
 
 func (cc *ContainerConfig) Volumes() []corev1.Volume {
