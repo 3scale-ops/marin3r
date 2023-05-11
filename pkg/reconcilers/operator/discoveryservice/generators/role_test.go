fc64ef0e56844ed4ec5130347736e3de8df7fd3b
Roi Vazquez <roivazquez@gmail.com>
2023-05-11 11:16:25 +0200

    Fix test

--- pkg/reconcilers/operator/discoveryservice/generators/role_test.go
+++ pkg/reconcilers/operator/discoveryservice/generators/role_test.go
@@ -7,6 +7,7 @@ import (
 	marin3rv1alpha1 "github.com/3scale-ops/marin3r/apis/marin3r/v1alpha1"
 	operatorv1alpha1 "github.com/3scale-ops/marin3r/apis/operator.marin3r/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
+	discoveryv1 "k8s.io/api/discovery/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/api/equality"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -66,6 +67,11 @@ func TestGeneratorOptions_Role(t *testing.T) {
 						APIGroups: []string{marin3rv1alpha1.GroupVersion.Group},
 						Resources: []string{rbacv1.ResourceAll},
 						Verbs:     []string{rbacv1.VerbAll},
+					},
+					{
+						APIGroups: []string{discoveryv1.SchemeGroupVersion.Group},
+						Resources: []string{"endpointslices"},
+						Verbs:     []string{"get", "list", "watch"},
 					},
 				},
 			},
